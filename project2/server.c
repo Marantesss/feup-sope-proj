e54c05fe6814da5f5fe689c42f726f75ababf12b
Marantesss <up201706917@fe.up.pt>
2019-05-14 11:26:40 +0100

    Server and User connection works perfectly

--- project2/server.c
+++ project2/server.c
@@ -15,63 +15,92 @@ PLAN OF ATTACK:
 6. server sends requested information through user fifo
 */
 
-void server_create() {
-   printf("Creating FIFO communication channels...");
-
-   if (mkfifo(SERVER_FIFO_PATH, 0666)) {
-      printf("\nfifo_server: Create attempt failed\n");
-      exit(RC_OTHER);
-   }
-
-   printf("\nChannels created.\nServer is online.\n");
-}
-
-void server_connect_user(int* fifo_server, int* fifo_user) {
-
-   *fifo_user = open(SERVER_FIFO_PATH, O_WRONLY | O_NONBLOCK); // | O_NONBLOCK makes so that it does not block
-
-   printf("Opening FIFO communication channels...");
-   // ---- opening server fifo -  does not wait for server
-   *fifo_server = open(SERVER_FIFO_PATH, O_RDONLY | O_NONBLOCK); // | O_NONBLOCK makes so that it does not block
-   if (*fifo_server == -1) {
-      printf("\nfifo_server: Open attempt failed.\n");
-      exit(RC_SRV_DOWN); 
-   }
-
-   /*
-   // ---- get user fifo path name 
-   char user_fifo_path[USER_FIFO_PATH_LEN] = USER_FIFO_PATH_PREFIX;
-   char user_fifo_path_sufix[WIDTH_ID + 1];
-   sprintf(user_fifo_path_sufix, "%d", getpid());
-   strcat(user_fifo_path, user_fifo_path_sufix);
-   printf("\nTESTING USER FIFO PATH: %s\n", user_fifo_path);
-
-   // ---- creating user fifo
-   if(mkfifo(user_fifo_path, 0666)) {
-      printf("\nfifo_user: Create attempt failed\n");
-   }
-   // ---- opening user fifo - waits for server to connect to user
-   *fifo_user = open(user_fifo_path, O_WRONLY);
-   printf("here %d\n", *fifo_user);
-   if (*fifo_user == -1) {
-      printf("\nfifo_user: Open attempt failed\n");
-      perror("error: ");
-      exit(RC_USR_DOWN);
-   }
-   */
-
-   sleep(10);
-
-   printf("\nChannels opened.\nServer is connected to user <pid_here>.\n");
-}
+int readline(int fd, char *str);
+void server_fifo_create(int* fifo_server);
+void user_fifo_create(char* user_fifo_path);
+void server_connect_user(int* fifo_server, int* fifo_user);
 
 int main() {
+   setbuf(stdout, NULL); // prints stuff without needing \n
    
    int fifo_user, fifo_server;
 
-   server_create();
+   server_fifo_create(&fifo_server);
 
-   server_connect_user(&fifo_server, &fifo_user);
+   while(1)
+      server_connect_user(&fifo_server, &fifo_user);
 
    return 0;
+}
+
+void server_fifo_create(int* fifo_server) {
+   printf("Creating server FIFO communication channels...");
+
+   // ---- creating server fifo
+   if (mkfifo(SERVER_FIFO_PATH, 0666)) {
+      // If the file already exists, delete it
+      unlink(SERVER_FIFO_PATH);
+      // Try again
+      if (mkfifo(SERVER_FIFO_PATH, 0666)) {
+         printf("\nfifo_server: Create attempt failed\n");
+         exit(-1);
+      }
+   }
+
+   // ---- opening server fifo
+   *fifo_server = open(SERVER_FIFO_PATH, O_RDONLY | O_NONBLOCK); // | O_NONBLOCK makes so that it does not block
+   if (*fifo_server == -1) {
+      printf("\nfifo_server: Open attempt failed.\n");
+      exit(-1);
+   }
+
+   printf("\nChannel created and connected.\nServer is online.\n");
+}
+
+void user_fifo_create(char* user_fifo_path) {
+   printf("\nCreating user FIFO communication channels...");
+
+   // ---- creating user fifo
+   if (mkfifo(user_fifo_path, 0666)) {
+      // If the file already exists, delete it
+      unlink(user_fifo_path);
+      // Try again
+      if (mkfifo(user_fifo_path, 0666)) {
+         printf("\nfifo_server: Create attempt failed\n");
+         exit(-2);
+      }
+   }
+
+   printf("\n%s channel created.\n", user_fifo_path);
+}
+
+void server_connect_user(int* fifo_server, int* fifo_user) {
+   // ---- get user fifo path name 
+   printf("\nWaiting for user data...");   
+   char user_fifo_path[USER_FIFO_PATH_LEN];
+   // wait for server fifo to have something
+   while(!readline(*fifo_server, user_fifo_path)) sleep(1);
+
+   // ---- create user fifo
+   user_fifo_create(user_fifo_path);
+
+   printf("\nOpening FIFO communication channels...");
+   // ---- opening user fifo - waits for user to connect to server
+   *fifo_user = open(user_fifo_path, O_WRONLY);
+   if (*fifo_user == -1) {
+      printf("\nfifo_user: Open attempt failed\n");
+      exit(-2);
+   }
+
+   printf("\nChannels opened.\nServer is connected to user <user_pid_here>.\n");
+}
+
+int readline(int fd, char *str) {
+   int n;
+
+   do { 
+      n = read(fd, str, 1);
+   } while (n > 0 && *str++ != '\0');
+
+   return (n>0); 
 }
--- project2/user.c
+++ project2/user.c
@@ -4,6 +4,7 @@
 #include <sys/file.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /*
 PLAN OF ATTACK:
@@ -13,44 +14,61 @@ PLAN OF ATTACK:
 3. user gets responde from user fifo
 */
 
-void user_connect_server(int* fifo_server, int* fifo_user) {
-
-    printf("Opening FIFO communication channels...");
-    // ---- opening server fifo -  does not wait for server
-    *fifo_server = open(SERVER_FIFO_PATH, O_WRONLY | O_NONBLOCK); // | O_NONBLOCK makes so that it does not block
-    if (*fifo_server == -1) {
-        printf("\nfifo_server: Open attempt failed.\n");
-        exit(RC_SRV_DOWN); 
-    }
-
-    // ---- get user fifo path name 
-    char user_fifo_path[USER_FIFO_PATH_LEN] = USER_FIFO_PATH_PREFIX;
-    char user_fifo_path_sufix[WIDTH_ID + 1];
-    sprintf(user_fifo_path_sufix, "%d", getpid());
-    strcat(user_fifo_path, user_fifo_path_sufix);
-    printf("\nTESTING USER FIFO PATH: %s\n", user_fifo_path);
-
-    // ---- creating user fifo
-    if(mkfifo(user_fifo_path, 0666)) {
-        printf("\nfifo_user: Create attempt failed\n");
-    }
-    // ---- opening user fifo - waits for server to connect to user
-    *fifo_user = open(user_fifo_path, O_RDONLY);
-    printf("here %d\n", *fifo_user);
-    if (*fifo_user == -1) {
-        printf("\nfifo_user: Open attempt failed\n");
-        perror("error: ");
-        exit(RC_USR_DOWN);
-    }
-
-    printf("\nChannels opened.\nUser %d connected to server.\n", getpid());
-}
+int readline(int fd, char *str);
+void user_connect_server(int* fifo_server, int* fifo_user);
+void get_user_fifo_path(char* user_fifo_path);
 
 int main() {
+   setbuf(stdout, NULL); // prints stuff without needing \n
 
-    int fifo_user, fifo_server;
+   int fifo_user, fifo_server;
 
-    user_connect_server(&fifo_user, &fifo_server);
+   user_connect_server(&fifo_user, &fifo_server);
 
-    return 0;
+   return 0;
+}
+
+void get_user_fifo_path(char* user_fifo_path) {
+   strcpy(user_fifo_path, USER_FIFO_PATH_PREFIX);
+   char user_fifo_path_sufix[WIDTH_ID + 1];
+   sprintf(user_fifo_path_sufix, "%d", getpid());
+   strcat(user_fifo_path, user_fifo_path_sufix);
+}
+
+void user_connect_server(int* fifo_server, int* fifo_user) {
+   printf("Opening FIFO communication channels...");
+
+   // ---- opening server fifo - does not wait for server
+   *fifo_server = open(SERVER_FIFO_PATH, O_WRONLY | O_NONBLOCK); // | O_NONBLOCK makes so that it does not block
+   if (*fifo_server == -1) {
+      printf("\nfifo_server: Open attempt failed.\n");
+      exit(RC_SRV_DOWN); 
+   }
+
+   // ---- opening user fifo - waits for server to create fifo and connect to user
+   // get user fifo path name 
+   char user_fifo_path[USER_FIFO_PATH_LEN];
+   get_user_fifo_path(user_fifo_path);
+   // sends user information to server
+   write(*fifo_server, user_fifo_path, USER_FIFO_PATH_LEN);
+   // wait for user fifo to be created
+   while(access(user_fifo_path, F_OK)) sleep(1);
+   // opening user fifo - waits for server to connect to user
+   *fifo_user = open(user_fifo_path, O_RDONLY);
+   if (*fifo_user == -1) {
+      printf("\nfifo_user: Open attempt failed\n");
+      exit(RC_USR_DOWN);
+   }
+
+   printf("\nChannels opened.\nUser %d connected to server.\n", getpid());
+}
+
+int readline(int fd, char *str) {
+   int n;
+
+   do { 
+      n = read(fd, str, 1); 
+   } while (n>0 && *str++ != '\0');
+
+   return (n>0); 
 }
