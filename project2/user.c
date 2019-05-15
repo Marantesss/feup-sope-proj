d37c81637b89d5070749894c6426a77f35162f25
Simao Santos <simao.p.m.santos@gmail.com>
2019-05-15 12:39:48 +0100

    Started user

--- project2/user.c
+++ project2/user.c
@@ -1,7 +1,32 @@
-#include <stdio.h>
+#include "utils.h"
 
+tlv_request_t *command = NULL;
 
 int main(int argc, char *argv[], char* envp[]) {
    printf("USER still in development\n!");
+
+   command = calloc(1, sizeof(tlv_request_t));
+
+   //-------parsing arguments
+   //for(int i = 1; i < argc - 1; i++){
+      switch (*argv[4]){
+      case '0': //create account
+         command->type = OP_CREATE_ACCOUNT;
+         break;
+      case '1':
+         command->type = OP_BALANCE;
+         if()
+         break;
+      case '2':
+         command->type = OP_TRANSFER;
+         break;
+      case '3':
+         command->type = OP_SHUTDOWN;
+         break; 
+      default:
+         break;
+      }
+   //}
+
    return 0;
 }
---
+++ project2/utils.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include "sope.h"
